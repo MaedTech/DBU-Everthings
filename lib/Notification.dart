9ee791229f11a810e10f2e9c0369ef181956e9a8
Yunus <aliyunus291@gmail.com>
2019-08-29 23:46:06 +0300

    notification page drawer added

--- lib/Notification.dart
+++ lib/Notification.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'aboutus.dart';
 
 
 
@@ -12,8 +13,155 @@ class _DbuEvethingNotState extends State<DbuEvethingNot> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      drawer: new Drawer(),
+      drawer: new Drawer(
+        elevation: (0.0),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              child: UserAccountsDrawerHeader(
+                accountName: Text('DBUEverything APP'),
+                accountEmail: Text('MaedTechnologies@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage(''),
+                ),
+              )
+              ,
+            ),
+          ListTile(
+            title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+            onTap: (){
+              Navigator.of(context).pop();
+              Navigator.push(context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => AboutPage()
+              ));
+            },
+          leading: Icon(
+              Icons.person,
+              color: Colors.blue,
+          ),
+          ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+
+            Divider(),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('write here ', style: TextStyle(fontSize: 18.0),),
+              onTap: (){
+                Navigator.of(context).pop();
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AboutPage()
+                    ));
+              },
+              leading: Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+            ),
+          ],
+
+        ),
+      ),
       appBar: AppBar(
+        elevation: 0,
+
       ),
         backgroundColor: Colors.blue,
     );
