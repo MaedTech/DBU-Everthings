86a4111024b0fc30899bf63ad10294e44cb8a134
Yunus <aliyunus291@gmail.com>
2019-08-29 10:23:32 +0300

    added drawer #13

---
+++ lib/Notification.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DbuEvethingNot extends StatefulWidget {
+  @override
+  _DbuEvethingNotState createState() => _DbuEvethingNotState();
+}
+
+class _DbuEvethingNotState extends State<DbuEvethingNot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      drawer: new Drawer(),
+      appBar: AppBar(
+      )
+    );
+  }
+}
